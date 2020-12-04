b7ba59709be029e2eca2cd2d95989bf220f5bf82
Maxim Fainberg <maximfainberg@gmail.com>
2020-12-04 19:24:26 +0100

    Day 4 - part 2

--- src/adv/t04.clj
+++ src/adv/t04.clj
@@ -20,10 +20,37 @@
   (clojure.set/subset? #{"byr" "iyr" "eyr" "hgt" "hcl" "ecl" "pid"} 
                        (set (keys pass))))
 
-(defn solve 
-  ([] (solve input))
-  ([passports] 
-    (->> passports 
-         (filter pass-ok?)
-         (count))))
+(defn check-passports [passports check?]
+  (->> passports 
+       (filter check?)
+       (count)))
+
+(defn solve [] 
+  (check-passports input pass-ok?))
+
+(defn to-num [s]
+  (if (and s (some? (re-seq #"^\d+$" s)))
+    (parse-int s)
+    -1))
+
+(defn pass-ok-two? [pass]
+  (let [birth-year (to-num (get pass "byr"))
+        issue-year (to-num (get pass "iyr"))
+        expiration-year (to-num (get pass "eyr"))
+        hair (get pass "hcl" "")
+        eye (get pass "ecl" "")
+        passport (get pass "pid" "")
+        [_ height-s unit] (first (re-seq #"(\d+)(in|cm)" (get pass "hgt" "")))
+        height (to-num height-s)] 
+    (and (>= birth-year 1920) (<= birth-year 2002)
+         (>= issue-year 2010) (<= issue-year 2020)
+         (>= expiration-year 2020) (<= expiration-year 2030)
+         (or (and (= unit "cm") (>= height 150) (<= height 193))
+             (and (= unit "in") (>= height 59) (<= height 76)))
+         (some? (re-seq #"^\d{9}$" passport))
+         (some? (re-seq #"^#[0-9a-f]{6}$" hair))
+         (some? (re-seq #"^(amb|blu|brn|gry|grn|hzl|oth)$" eye)))))
+
+(defn solve2 []
+  (check-passports input pass-ok-two?))
 
